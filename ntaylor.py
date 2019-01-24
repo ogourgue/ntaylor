d52f99eca6e20e4b7cac963ccc9c3c46f1bb1c3e
Olivier Gourgue <ogourgue@gmail.com>
2019-01-24 17:25:49 +0100

    simplify the approach and better comment the functions

--- ntaylor.py
+++ ntaylor.py
@@ -2,7 +2,21 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+################################################################################
+
 def get_stat(x0, x):
+
+  """
+  calculate different statistics (mean, bias, root mean square error, centered root mean square error, standard deviation, correlation coefficient) from x (test dataset), with regards to x0 (reference dataset)
+
+  input:
+  x0: array of shape (n)
+  x: array of shape (m, n)
+
+  output:
+  stat0: reference statistic array of shape (6)
+  stat: statistic array of shape (m, 6)
+  """
 
   # number of data
   n = x0.shape[0]
@@ -14,7 +28,7 @@ def get_stat(x0, x):
   sigma0 = np.sqrt(np.sum((x0 - s0) ** 2.) / n)
 
   # all statistics
-  stat0 = [s0, 0., 0., 0., sigma0, 1.]
+  stat0 = np.array([s0, 0., 0., 0., sigma0, 1.])
 
   # initialization
   stat = np.zeros((x.shape[0], 6))
@@ -54,7 +68,20 @@ def get_stat(x0, x):
   return stat0, stat
 
 
+################################################################################
+
 def get_statn(x0, x):
+
+  """
+  calculate different statistics (mean, bias, root mean square error, normalized centered root mean square error, normalized standard deviation, correlation coefficient) from x (test dataset), with regards to x0 (reference dataset)
+
+  input:
+  x0: array of shape (n)
+  x: array of shape (m, n)
+
+  output:
+  statn: normalized statistic array of shape (m, 6)
+  """
 
   # all statistics
   [stat0, stat] = get_stat(x0, x)
@@ -67,20 +94,30 @@ def get_statn(x0, x):
   return statn
 
 
-def diagn(statn, prop, legend, title = '', no_crmse_axis = False, \
-          no_std_axis = False):
+################################################################################
 
-  # maximum standard deviation to plot
-  sigma_lim = 1.5
+def diagn(ax, statn, prop, legend, title = '', sigma_lim = 1.5, \
+          sigma_color = 'k', crmse_color = 'C0', r_color = 'C2', \
+          no_sigma_axis = False, no_crmse_axis = False):
 
-  # default colors
-  sigma_col = 'k'
-  crmse_col = 'C0'
-  r_col = 'C2'
+  """
+  create a normalized taylor diagram
 
-  # create figure
-  fig = plt.figure()
-  ax = plt.gca()
+  input:
+  ax: figure axes
+  statn: normalized statistic array of shape (m, 6) from get_statn()
+  prop: 2d list of shape (m, 3), with prop[i] = [marker, markerfacecolor, markeredgecolor] of data i
+  legend: 2d list of shape (n, 3), with legend[i] = [marker, markerfacecolor, markeredgecolor, legend] of data i in the legend: for flexibility sake, data in legend can be different from data in diagram
+
+  # input (not mandatory):
+  title: title on top of the diagram
+
+
+  output:
+  ax: figure axes
+  """
+
+  # initialize figure
   plt.axis('square')
   plt.xlim([0., 1.01 * sigma_lim])
   plt.ylim([0., 1.01 * sigma_lim])
@@ -93,12 +130,12 @@ def diagn(statn, prop, legend, title = '', no_crmse_axis = False, \
     x = rho * np.cos(theta);
     y = rho * np.sin(theta);
     if rho == 1.:
-      plt.plot(x, y, color = sigma_col, linewidth = 1.)
+      plt.plot(x, y, color = sigma_color, linewidth = 1.)
     else:
-      plt.plot(x, y, '--', color = sigma_col, linewidth = .5)
+      plt.plot(x, y, '--', color = sigma_color, linewidth = .5)
   plt.yticks(np.arange(0., sigma_lim + .125, .25))
-  ax.spines['left'].set_color(sigma_col)
-  if no_std_axis:
+  ax.spines['left'].set_color(sigma_color)
+  if no_sigma_axis:
     yticks = ax.get_yticks()
     yticklabels = []
     for i in range(len(yticks)):
@@ -116,12 +153,12 @@ def diagn(statn, prop, legend, title = '', no_crmse_axis = False, \
     # remove what is out of sigma_lim circle
     y = y[x <= (1. + sigma_lim ** 2. - rho ** 2) * 0.5]
     x = x[x <= (1. + sigma_lim ** 2. - rho ** 2) * 0.5]
-    plt.plot(x, y, '--', color = crmse_col, linewidth = .5)
+    plt.plot(x, y, '--', color = crmse_color, linewidth = .5)
   plt.xticks(np.arange(0., 1.125, .25), \
              ['1.00', '0.75', '0.50', '0.25', '0.00'])
-  ax.spines['bottom'].set_color(crmse_col)
-  ax.tick_params(axis = 'x', colors = crmse_col)
-  ax.xaxis.label.set_color(crmse_col)
+  ax.spines['bottom'].set_color(crmse_color)
+  ax.tick_params(axis = 'x', colors = crmse_color)
+  ax.xaxis.label.set_color(crmse_color)
   if no_crmse_axis:
     xticks = ax.get_xticks()
     xticklabels = []
@@ -135,46 +172,48 @@ def diagn(statn, prop, legend, title = '', no_crmse_axis = False, \
   for theta in [.1, .2, .3, .4, .5, .6, .7, .8, .9, .95, .99]:
     x = sigma_lim * theta
     y = sigma_lim * np.sin(np.arccos(theta))
-    plt.plot([0., x], [0., y], ':', color = r_col, linewidth = .5)
+    plt.plot([0., x], [0., y], ':', color = r_color, linewidth = .5)
     if theta < .95:
       coef = 1.04
     else:
       coef = 1.05
     ax.text(coef * x, coef * y, str(theta), verticalalignment = 'center', \
             horizontalalignment = 'center', \
-            rotation = np.degrees(np.arccos(theta)), color = r_col)
+            rotation = np.degrees(np.arccos(theta)), color = r_color)
   x = 1.1 * sigma_lim * np.cos(np.pi / 4.)
   y = 1.1 * sigma_lim * np.sin(np.pi / 4.)
   ax.text(x, y, 'correlation coefficient', verticalalignment = 'center', \
           horizontalalignment = 'center', \
-          rotation = -45., color = r_col)
+          rotation = -45., color = r_color)
 
   # target point
   plt.plot(1., 0., 'ko')
 
-  # for each data type
-  for i in range(len(statn)):
-    rho = statn[i][:, 4]
-    theta = np.arccos(statn[i][:, 5])
-    x = rho * np.cos(theta)
-    y = rho * np.sin(theta)
-    for j in range(len(x)):
-      if rho[j] < 1.5:
-        plt.plot(x[j], y[j], marker = prop[i][j][0], \
-                 markerfacecolor = prop[i][j][1], markeredgecolor = 'k')
+  # data points
+  rho = statn[:, 4]
+  theta = np.arccos(statn[:, 5])
+  x = rho * np.cos(theta)
+  y = rho * np.sin(theta)
+  for i in range(len(x)):
+    if rho[i] < 1.5:
+      plt.plot(x[i], y[i], marker = prop[i][0], \
+                           markerfacecolor = prop[i][1], \
+                           markeredgecolor = prop[i][2])
 
   # legend
   for i in range(len(legend)):
-    plt.plot(-1., -1., '.', marker = legend[i][0], markerfacecolor = legend[i][1], \
-             markeredgecolor = legend[i][2], label = legend[i][3])
+    plt.plot(-1., -1., '.', marker = legend[i][0], \
+                            markerfacecolor = legend[i][1], \
+                            markeredgecolor = legend[i][2], \
+                            label = legend[i][3])
   if len(legend) > 0:
     if len(legend) < 12:
-      plt.legend(bbox_to_anchor=(1.01, 1.), loc='upper left')
+      plt.legend(bbox_to_anchor=(1.01, 1.), loc = 'upper left')
     else:
-      plt.legend(bbox_to_anchor=(1.03, 1.), loc='upper left')
+      plt.legend(bbox_to_anchor=(1.03, 1.), loc = 'upper left')
 
   # title
   if title is not '':
     plt.text(.75, 1.65, title, horizontalalignment = 'center')
 
-  return fig
+  return ax
